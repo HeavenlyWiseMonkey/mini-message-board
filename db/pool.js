b35bc1fe627b8aa75dc2ec3ff356bbc147d01cd8
odin <139291798+minecrafter021345@users.noreply.github.com>
2024-12-07 17:02:29 -0800

    Revert "Fix database connection to platform"
    This reverts commit b4c93c34e08dca0e7b20a227fea68a513b8c148a.

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-    connectionString: (process.env.CONNECTIONSTRING) ? process.env.CONNECTIONSTRING :
-    `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`,
+    connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`,
 });
--- db/populatedb.js
+++ db/populatedb.js
@@ -31,8 +31,7 @@ VALUES
 async function main() {
     console.log('seeding...');
     const client = new Client({
-        connectionString: (process.env.CONNECTIONSTRING) ? process.env.CONNECTIONSTRING : 
-        `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`,
+        connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`,
     });
     await client.connect();
     await client.query(SQL);
